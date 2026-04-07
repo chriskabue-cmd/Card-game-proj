c0ffc0c9e606ccc18ab62879664afadaecc5e0f6
Chris kabue <chris.kabue@student.moringaschool.com>
2026-04-07 15:14:27 +0300

    changes on card and deck

---
+++ models/deck.py
@@ -0,0 +1,17 @@
+import random
+from models.card import Card
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw(self):
+        return self.cards.pop() if self.cards else None
